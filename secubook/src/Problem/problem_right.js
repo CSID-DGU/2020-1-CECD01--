261332eb86f23a87019e17ea67961395a16fb146
leeseungmin <lsm0341@naver.com>
2020-10-15 21:37:56 +0900

    problem 페이지

--- secubook/src/Problem/problem_right.js
+++ secubook/src/Problem/problem_right.js
@@ -8,8 +8,49 @@ class Problem_right extends Component {
     render() {
         return (
             <div>
-                hello
-            </div>
+                <div class="row">
+
+                    <div className="box">
+                        <b className="bold">
+                            <div className="problem_num">1234</div>
+                            <div className="problem_name">문제 이름</div>
+                            <div className="problem_complete">완료</div>
+                        </b>
+                        <div className="problem_type">크로스사이트스크립트</div>
+                        <br></br>
+                        <div>
+                            <div className="problem_solve">235명 완료</div>
+                            <div className="problem_percentage">정답률 65%</div>
+                        </div>
+                    </div>
+
+                    <div className="box">
+                        <b className="bold">
+                            <div className="problem_num">1234</div>
+                            <div className="problem_name">문제 이름</div>
+                        </b>
+                        <div className="problem_type">크로스사이트스크립트</div>
+                        <br></br>
+                        <div>
+                            <div className="problem_solve">235명 완료</div>
+                            <div className="problem_percentage">정답률 65%</div>
+                        </div>
+                    </div>
+
+                    <div className="box">
+                        <b className="bold">
+                            <div className="problem_num">1234</div>
+                            <div className="problem_name">문제 이름</div>
+                        </b>
+                        <div className="problem_type">크로스사이트스크립트</div>
+                        <br></br>
+                        <div>
+                            <div className="problem_solve">235명 완료</div>
+                            <div className="problem_percentage">정답률 65%</div>
+                        </div>
+                    </div>
+                </div>
+            </div >
         )
     }
 
